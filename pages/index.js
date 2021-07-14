863332b52a79237e7fb70e852aacec5b04dc9dea
Patrick Cruz <patrickezequielscruz@gmail.com>
2021-07-13 22:55:45 -0300

    Estrutura inicial da pagina do orkut completa

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,70 @@
-import styled from 'styled-components'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+import MainGrid from '../src/components/MainGrid'
+import Box from '../src/components/Box'
+import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations'
+import { AlurakutMenu, OrkutNostalgicIconSet } from '../src/lib/OrkutComons'
+
+function ProfileSideBar(propriedades) { 
+  return (
+    <Box >
+      <img src={`https://github.com/${propriedades.gitHubUser}.png`} style={{ borderRadius: '8px' }}/>
+    </Box>
+  )
+}
 
 export default function Home() {
-  return <Title>My page</Title>
+  const usuarioX = 'patrickzequiel'
+
+  const pessoasFavoritas = [
+    'juunegreiros', 
+    'omariosouto', 
+    'peas', 
+    'rafaballerini', 
+    'marcobrunodev',
+    'felipefialho'
+  ]
+
+  return (
+    <>
+        <AlurakutMenu />
+        <MainGrid>
+        <div className="profileArea" style={{ gridArea: 'profileArea' }}>
+          <ProfileSideBar gitHubUser={usuarioX}/>
+        </div>
+        <div className="welcomeArea" style={{ gridArea: 'welcomeArea' }}>
+        <Box>
+          <h1 className="title">
+            Bem Vindo
+          </h1>
+
+          <OrkutNostalgicIconSet />
+        </Box>
+        </div>
+        <div className="profileRelationsArea"  style={{ gridArea: 'profileRelationsArea' }}>
+        <ProfileRelationsBoxWrapper>
+          <h2 className="smallTitle">
+            Pessoas da comunidade ({pessoasFavoritas.push.length})
+            </h2>
+
+          <ul>
+          {pessoasFavoritas.map((itemAtual) => {
+            return (
+              <li>
+                <a href={`/users/${itemAtual}`} key={itemAtual}>
+                <img src={`https://github.com/${itemAtual}.png`} style={{ borderRadius: '8px' }}/>
+                <span>{itemAtual}</span>
+              </a>
+              </li>
+
+            )
+          })} 
+          </ul>
+        </ProfileRelationsBoxWrapper>
+        <Box>
+          Comunidades
+        </Box>
+        </div>
+      </MainGrid>
+    </>
+    )
 }
---
+++ src/components/Box/index.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components'
+
+const Box = styled.div`
+    background: #FFFFFF;
+    border-radius: 8px;
+
+    padding: 16px;
+
+    margin-bottom: 10px;
+    .boxLink {
+        font-size: 14px;
+        color: #2E7084;
+        text-decoration: none;
+        font-weight: 800;
+    }
+    .title {
+        font-size: 32px;
+        font-weight: 400;
+        margin-bottom: 200px;
+    }
+    .subTitle {
+        font-size: 10px;
+        font-weight: 400;
+        margin-bottom: 20px;
+    }
+`
+
+export default Box
